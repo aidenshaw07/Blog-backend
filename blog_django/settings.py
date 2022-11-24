d12a4d68f1600858b4a8a2c5faeecd85a999c126
Mehmet Tansu Soydan <aidenshaw.leo@gmail.com>
2022-11-24 15:39:12 -0500

    deployment test 6

--- blog_django/settings.py
+++ blog_django/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get(
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 

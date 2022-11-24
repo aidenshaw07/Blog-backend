b76dbfedab846469ed1d806b8cbc4093272fa2df
Mehmet Tansu Soydan <aidenshaw.leo@gmail.com>
2022-11-24 15:57:03 -0500

    deployment test 9

--- blog_django/urls.py
+++ blog_django/urls.py
@@ -23,6 +23,7 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
+    path('/', admin.site.urls),
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
